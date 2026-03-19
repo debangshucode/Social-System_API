62f20a9b64ac3d91adeade99e57fab4b2f1a57aa
Debangshu Dey <deydebangshu51@gmail.com>
2026-03-19 14:34:59 +0530

    typeorm configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,39 @@ import { ProfilesModule } from './profiles/profiles.module';
 import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
 import { LikesModule } from './likes/likes.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, ProfilesModule, PostsModule, CommentsModule, LikesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+
+        url: process.env.DATABASE_URL || undefined,
+
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+
+        autoLoadEntities: true,
+
+        migrations: ['dist/db/migrations/*.js'],
+        migrationsRun: process.env.NODE_ENV == 'production',
+
+        synchronize: false,
+
+        ssl: process.env.DATABASE_URL ?
+          { rejectUnauthorized: false }
+          : false,
+      }),
+    }), UsersModule, ProfilesModule, PostsModule, CommentsModule, LikesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/data-source.ts
@@ -0,0 +1,26 @@
+import 'reflect-metadata';
+import * as dotenv from 'dotenv'
+import { DataSource } from 'typeorm';
+
+dotenv.config()
+
+export const AppDataSource = new DataSource({
+    type:'postgres',
+
+    url:process.env.DATABASE_URL || undefined,
+
+    host: process.env.DATABASE_URL ? undefined : process.env.DB_HOST,
+    port: process.env.DATABASE_URL ? undefined : Number(process.env.DB_PORT),
+    username: process.env.DATABASE_URL ? undefined : process.env.DB_USERNAME,
+    password: process.env.DATABASE_URL ? undefined : process.env.DB_PASSWORD,
+    database: process.env.DATABASE_URL ? undefined : process.env.DB_NAME,
+
+    ssl: process.env.DATABASE_URL ?
+    {rejectUnauthorized:false} :false,
+
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    migrations: [__dirname + '/../db/migrations/*{.ts,.js}'],
+
+    synchronize:false
+
+})
