880c4fd023b79118ac67cfb3a7bddc2adb082701
Debangshu Dey <deydebangshu51@gmail.com>
2026-03-23 12:56:13 +0530

    changes profile enity & migration

--- src/db/migrations/1773911185759-create-profile-table.ts
+++ src/db/migrations/1773911185759-create-profile-table.ts
@@ -5,55 +5,63 @@ export class CreateProfileTable1773911185759 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name:'profiles',
-                columns:[
+                name: 'profiles',
+                columns: [
                     {
-                        name:'id',
-                        type:'int',
-                        isPrimary:true,
-                        isGenerated:true,
-                        generationStrategy:'increment'
+                        name: 'id',
+                        type: 'int',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment'
                     },
                     {
-                        name:'user_name',
-                        type:'varchar',
-                        isUnique:true,
+                        name: 'user_name',
+                        type: 'varchar',
+                        isUnique: true,
                     },
                     {
-                        name:'bio',
-                        type:'text',
+                        name: 'bio',
+                        type: 'text',
+                        isNullable: true,
                     },
                     {
-                        name:'avatar_url',
-                        type:'varchar'
+                        name: 'avatar_url',
+                        type: 'text',
+                        isNullable: true,
                     },
                     {
-                        name:'created_at',
-                        type:'timestamp',
-                        default:'now()'
+                        name: 'cloudinary_public_id',
+                        type: 'varchar',
+                        isNullable: true,
+                        length: '300',
                     },
                     {
-                        name:'deleted_at',
-                        type:'timestamp',
-                        isNullable:true
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()'
                     },
                     {
-                        name:'updated_at',
-                        type:'timestamp',
-                        default:'now()',
-                        onUpdate:'CURRENT_TIMESTAMP'
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                        onUpdate: 'CURRENT_TIMESTAMP'
                     },
                     {
-                        name:'user_id',
-                        type:'int',
-                        isUnique:true,
+                        name: 'deleted_at',
+                        type: 'timestamp',
+                        isNullable: true
+                    },
+                    {
+                        name: 'user_id',
+                        type: 'int',
+                        isUnique: true,
                     }
                 ],
-                foreignKeys:[
+                foreignKeys: [
                     {
-                        columnNames:['user_id'],
-                        referencedTableName:'users',
-                        referencedColumnNames:['id'],
+                        columnNames: ['user_id'],
+                        referencedTableName: 'users',
+                        referencedColumnNames: ['id'],
                     }
                 ]
             })
--- src/profiles/entities/profile.entity.ts
+++ src/profiles/entities/profile.entity.ts
@@ -1,8 +1,8 @@
 import { Entity, Column, PrimaryGeneratedColumn, DeleteDateColumn, JoinColumn, OneToOne, OneToMany, CreateDateColumn, UpdateDateColumn } from "typeorm";
 import { User } from "../../users/entities/user.entity";
 import { Post } from "../../posts/entities/post.entity";
-import { Comment } from "src/comments/entities/comment.entity";
-import { Like } from "src/likes/entities/like.entity";
+import { Comment } from "../../comments/entities/comment.entity";
+import { Like } from "../../likes/entities/like.entity";
 @Entity('profiles')
 export class Profile {
 
@@ -15,9 +15,10 @@ export class Profile {
     user_name: string
 
     @Column({
-        type: 'text'
+        type: 'text',
+        nullable: true
     })
-    bio: string
+    bio: string | null
 
     @Column({ type: 'text', nullable: true })
     avatar_url: string | null;
@@ -28,11 +29,11 @@ export class Profile {
     @CreateDateColumn()
     created_at: Date
 
-    @DeleteDateColumn()
-    deleted_at: Date
-
     @UpdateDateColumn()
     updated_at: Date
+
+    @DeleteDateColumn()
+    deleted_at: Date
 
     @OneToOne(() => User, (user) => user.profile)
     @JoinColumn({ name: 'user_id' })
