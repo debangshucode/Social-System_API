d5691f022839aae90ca16a1f2587ab568a72bcdc
Debangshu Dey <deydebangshu51@gmail.com>
2026-03-25 15:29:33 +0530

    fix logout and refresh token behaviour

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -72,7 +72,7 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'strict',
-      path: '/auth/refresh',
+      path: '/auth',
     });
     return { message: 'Logged Out' };
   }
